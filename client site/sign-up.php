8d4c86a84e90223daf89aeb0c2d7b033477d6209
sgb999 <59648138+sgb999@users.noreply.github.com>
2020-01-10 16:06:12 +0000

    Add files via upload

--- client site/sign-up.php
+++ client site/sign-up.php
@@ -1,28 +1,5 @@
 <?php
 $view = new stdClass();
-$view->pageTitle = 'Sign-up';
+$view->pageTitle = 'Register a user';
 session_start();
-require_once('Models/MySQL.php');
-require 'Controller/validation.php';
-if (isset($_POST['submit'])) {
-    if (validate_length($_POST['user_name'], 3, 20) && validate_length($_POST['user_pass'], 8, 255) && validate_email($_POST['user_email'])) {
-        if (isset($_POST['submit']))
-        {
-            require_once('Models/MySQL.php');
-            $MySQL = new MySQL(); // creates a MySQL class object
-            $result = $MySQL->addUser($_POST['username'], $_POST['pass'], $_POST['email']); // passes data to the addUser method
-            if (!$result) {
-                echo 'something went wrong while registering. Please try again later.';
-            }
-            else
-            {
-                echo 'user successfully added';
-            }
-        }
-
-    }
-    else {
-        echo 'You have entered the incorrect information';
-    }
-}
-require_once('Views/sign-up.phtml');
+require_once('Controller/sign-up.php');

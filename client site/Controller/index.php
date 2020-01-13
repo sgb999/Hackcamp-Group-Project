3744b075ed002c4de67521afcb9946427e6c6652
sgb999 <59648138+sgb999@users.noreply.github.com>
2020-01-13 15:40:22 +0000

    Add files via upload

--- client site/Controller/index.php
+++ client site/Controller/index.php
@@ -2,14 +2,10 @@
 require_once ('Models/MySQL.php');
 $MySQL = new MySQL();
 $ID = $_SESSION['userID'];
-$allMySQL = $MySQL->getMyProject($ID);
+$result = $MySQL->getTeamNumber($ID);
+$teamNumbers = $result['teamNumber'];
+for($i =0 ; $i < $result; $i++) {
+    $allMySQL = $MySQL->getMyProject($teamNumbers[$i]);
+}
 $view->MySQL = $allMySQL;
 require_once('Views/index.phtml');
-//$view = new stdClass();
-//$view->pageTitle = 'Topics';
-//require_once ('Models/MySQL.php');
-//$cat_id=implode($_GET);
-//$MySQL = new MySQL();
-
-//$allMySQL = $MySQL->getSelectedTopics($cat_id);
-//$view->MySQL = $allMySQL;

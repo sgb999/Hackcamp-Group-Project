eb6ff848c1655c78a62d056d8d24cf66af12e327
sgb999 <59648138+sgb999@users.noreply.github.com>
2020-01-15 16:44:35 +0000

    Add files via upload

--- client site/Controller/create-project.php
+++ client site/Controller/create-project.php
@@ -2,12 +2,34 @@
 require_once ('Models/MySQL.php');
 $projectID=implode($_GET);
 $MySQL = new MySQL();
-//$allMySQL = $MySQL->getSelectedTopics($projectID);
+
 //$view->MySQL = $allMySQL;
-    //$view->MySQL = $MySQL->getCat();
-   // if (isset($_POST['submit'])) {
-      //  $result = $MySQL->setTopics($_POST['topic_subject'], $_POST['cat_id']);
-        //$result = $MySQL->setPosts($_POST['post_content'], $_POST['topic_subject']);
-      //  header("Refresh:0");
-   // }
+   if (isset($_POST['submit'])) {
+       $result = $MySQL->checkClient($_POST['clientName']);
+       if(!$result)
+       {
+           $result = $MySQL->createClient($_POST['clientName']);
+       }
+       $result = $MySQL->getClientID($_POST['clientName']);
+       $number = $MySQL->getTeamNumbers();
+       do{
+           $num = (rand(1, 99999999999));
+           $match = false;
+           foreach ($number as $dbnum){
+               if ($num == $dbnum){
+                   $match = true;
+               }
+               //array_push($allMySQL )
+           }
+       }while($match == true);
+       $array = explode(', ', $_POST['usernames']);
+       $arrayIDs =[];
+       foreach ($array as $username){
+           $arrayIDs = $MySQL->getUserID($username);
+       }
+       foreach ($arrayIDs as $user) {
+           $something = $MySQL->createTeam($user, $num);
+       }
+       $somethingElse = $MySQL->createProject($_POST['projectName'], $result, $num);
+    }
     require_once('Views/create-project.phtml');

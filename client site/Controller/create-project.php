042ea8f3c9589ffa5c5a966b10ca34b7daba7fa0
j-j-cramsie <59648243+j-j-cramsie@users.noreply.github.com>
2020-01-16 12:43:26 +0000

    Add files via upload

--- client site/Controller/create-project.php
+++ client site/Controller/create-project.php
@@ -6,14 +6,14 @@ $MySQL = new MySQL();
 //$view->MySQL = $allMySQL;
    if (isset($_POST['submit'])) {
        $result = $MySQL->checkClient($_POST['clientName']);
-       if(!$result)
+       if($result[0] == null)
        {
            $result = $MySQL->createClient($_POST['clientName']);
        }
        $result = $MySQL->getClientID($_POST['clientName']);
        $number = $MySQL->getTeamNumbers();
        do{
-           $num = (rand(1, 99999999999));
+           $num = (rand(1, 247483647));
            $match = false;
            foreach ($number as $dbnum){
                if ($num == $dbnum){
@@ -28,8 +28,8 @@ $MySQL = new MySQL();
            $arrayIDs = $MySQL->getUserID($username);
        }
        foreach ($arrayIDs as $user) {
-           $something = $MySQL->createTeam($user, $num);
+           $something = $MySQL->createTeam($user['userID'], $num);
        }
-       $somethingElse = $MySQL->createProject($_POST['projectName'], $result, $num);
+       $somethingElse = $MySQL->createProject($_POST['projectName'], $result[0], $num);
     }
     require_once('Views/create-project.phtml');

9f7bd4f444001830b428bd8c248f3f0c4e18a5df
j-j-cramsie <59648243+j-j-cramsie@users.noreply.github.com>
2020-01-16 10:56:27 +0000

    Add files via upload

--- client site/Controller/index.php
+++ client site/Controller/index.php
@@ -1,14 +1,11 @@
 <?php
-require_once ('Models/MySQL.php');
-$MySQL = new MySQL();
+require_once ('Models/ProjectsDataSet.php');
+require_once ('Models/Projects.php');
+
+$projectsDataSet = new ProjectsDataSet();
 $ID = $_SESSION['userID'];
-$result = $MySQL->getTeamNumbers($ID);
 
-$allMySQL = [];
+$dataset = $projectsDataSet->fetchDataByUserID($ID);
 
-for($i =0 ; $i < count($result); $i++) {
-    array_push($allMySQL, $MySQL->getMyProject($result[$i]['teamNumber'], $ID));
-    // var_dump($allMySQL);
-}
-$view->MySQL = $allMySQL;
+$view->MySQL = $dataset;
 require_once('Views/index.phtml');

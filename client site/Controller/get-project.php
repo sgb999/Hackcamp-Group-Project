fa93f2ab34b6de0c48dfe705eff8234eecc990cb
sgb999 <59648138+sgb999@users.noreply.github.com>
2020-01-16 16:00:43 +0000

    Add files via upload

--- client site/Controller/get-project.php
+++ client site/Controller/get-project.php
@@ -1,7 +1,10 @@
 <?php
-require_once ('Models/MySQL.php');
+require_once ('Models/UsersDataSet.php');
+require_once ('Models/User.php');
 $projectID=implode($_GET);
-$MySQL = new MySQL();
+$projectsDataSet = new UsersDataSet();
+$array = $projectsDataSet->fetchDataByProject($projectID);
+$view->MySQL = $array;
 //$allMySQL = $MySQL->getTimesheetsgraphs($projectID);
 //$view->MySQL = $allMySQL;
 require_once('Views/get-project.phtml');

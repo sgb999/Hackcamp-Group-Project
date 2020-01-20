fd1e6db472c8cf3f2ec3b07b88626dd4b35b4427
j-j-cramsie <59648243+j-j-cramsie@users.noreply.github.com>
2020-01-20 12:24:44 +0000

    Add files via upload

--- client site/Controller/get-project.php
+++ client site/Controller/get-project.php
@@ -1,10 +1,56 @@
 <?php
 require_once ('Models/UsersDataSet.php');
 require_once ('Models/User.php');
-$projectID=implode($_GET);
-$projectsDataSet = new UsersDataSet();
-$array = $projectsDataSet->fetchDataByProject($projectID);
-$view->MySQL = $array;
+require_once ('Models/DBTimesheetEntryDataSet.php');
+require_once ('Models/DBTimesheetEntry.php');
+$projectID=implode($_GET); //TODO: check if project ID is empty
+$ID = $_SESSION['UserID'];
+$level = $_SESSION['userLevel'];
+
+$usersDataSet = new UsersDataSet();
+if ($level = 0){
+    $userArray = $ID;
+} else {
+    $userArray = $usersDataSet->fetchDataByProject($projectID);
+}
+$view->MySQL = $userArray;
+
+//store distance for user from timesheet entries
+$userDistanceArray = Array();
+//store worked hours for user from timesheet entries
+$userTimeArray = Array();
+//Class to return queries by timesheet entries
+$entryDataSet = new DBTimesheetEntryDataSet();
+
+// for each user associated with this project
+foreach($userArray as $user){
+    // create arrays for the distance and time
+    $distanceArray = Array();
+    $timeWorkedArray = Array();
+    // get user ID
+    $userID = $user->getUserID();
+    // get entries
+    // TODO: check if the timesheet exists
+    $timesheetEntries = $entryDataSet->fetchDataByProjectAndUser($projectID,$userID);
+
+    //seed arrays with null values
+    for ($i = 1; $i <= 31; $i++){
+        $distanceArray[$i] = 0;
+        $timeWorkedArray[$i] = 0;
+    }
+
+    // for each entry associated with this user, store content in an array by date
+    foreach ($timesheetEntries as $entry){
+        $distanceArray[$entry->getDate()] = $entry->getDistance();
+        $timeWorkedArray[$entry->getDate()] = $entry->getWorkingHours();
+    }
+    // store the arrays as a string in an array sorted by user
+    $userDistanceArray[$user->getUserID()] = implode(', ', $distanceArray);
+    $userTimeArray[$user->getUserID()] = implode(', ', $timeWorkedArray);
+}
+
+// TODO: send $userDistanceArray and $userTimeArray to the view
+
 //$allMySQL = $MySQL->getTimesheetsgraphs($projectID);
 //$view->MySQL = $allMySQL;
 require_once('Views/get-project.phtml');
